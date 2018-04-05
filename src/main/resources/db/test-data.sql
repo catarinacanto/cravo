33ccd45e2941a5d1a4a113ca8556b91865b26471
marianopicco <marianopicco@gmail.com>
2018-04-05 22:26:32 +0100

    more test cases for db

--- src/main/resources/db/test-data.sql
+++ src/main/resources/db/test-data.sql
@@ -1,3 +1,7 @@
 DELETE FROM user;
 INSERT INTO user(ID, USERNAME, NAME, PASSWORD, EMAIL, STREETADDRESS, CITY, COUNTRY) VALUES
-  (1, 'hellofriends', 'John', 'bigman', 'johnnyjohn@jesus.maria', 'Rua dos paus 1', 'Paugrande', 'Paumaior');
+  (1, 'hellofriends', 'John', 'bigman', 'johnnyjohn@jesus.maria', 'Rua dos paus 1', 'Paugrande', 'Paumaior'),
+  (2, 'nabo123', 'Jose', 'asdf', 'jesus@jesus.maria', 'Rua das meias 2', 'Bagdad', 'Iraq'),
+  (3, 'loser', 'Bigest', 'fuck', 'loser@jesus.maria', 'Rua dos bananas 3', 'Portalegre', 'Portugal'),
+  (4, '1234', 'Maria', 'qwerty', 'maria@jesus.maria', 'Rua das batatas 1', 'Sao Paulo', 'Brasil'),
+  (5, 'richard', 'Richard', 'virgin', 'richard@jesus.maria', 'Rua dos lilases', 'Almada', 'Portugal'),
