95d77dfa3a998e751527e0526886649bc39508ff
Martelo <martelo300@hotmail.com>
2018-04-05 19:18:28 +0100

    intro javafx

---
+++ src/main/java/org/academiadecodigo/hackathon/cravo/javafx/Login.java
@@ -0,0 +1,33 @@
+package org.academiadecodigo.hackathon.cravo.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+
+        gridPane.setGridLinesVisible(false);
+
+        Scene scene = new Scene(gridPane, 500, 500);
+
+        Label title = new Label("xxxxxxx");
+
+
+        Navigation navigation = Navigation.getInstance();
+        navigation.setStage(primaryStage);
+        navigation.load("main");
+    }
+}
---
+++ src/main/java/org/academiadecodigo/hackathon/cravo/javafx/Navigation.java
@@ -0,0 +1,46 @@
+package org.academiadecodigo.hackathon.cravo.javafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class Navigation {
+
+    private static final String VIEW_PATH = "/org/academiadecodigo/hackathon/cravo/javafx";
+    private static Navigation instance;
+    private Stage stage;
+    private LinkedList<Scene> history = new LinkedList<Scene>();
+
+
+    public static Navigation getInstance() {
+
+    }
+
+    public void load(String view) {
+
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(VIEW_PATH + view + ".fxml"));
+            Parent root = fxmlLoader.load();
+
+            Scene scene = new Scene(root, 1000, 640);
+
+            stage.setScene(scene);
+            stage.show();
+
+            history.push(scene);
+
+        } catch (IOException e) {
+            System.err.println("Unable to load view " + view + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
