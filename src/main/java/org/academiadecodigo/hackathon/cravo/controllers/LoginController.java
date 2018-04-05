b7c6a8ff14224bcf1dc866a2639561e5bf974629
Catarina Canto <catarinaccanto@gmail.com>
2018-04-05 20:38:34 +0100

    added login controller methods

--- src/main/java/org/academiadecodigo/hackathon/cravo/controllers/LoginController.java
+++ src/main/java/org/academiadecodigo/hackathon/cravo/controllers/LoginController.java
@@ -1,4 +1,31 @@
 package org.academiadecodigo.hackathon.cravo.controllers;
 
+import org.academiadecodigo.hackathon.cravo.services.UserService;
+
 public class LoginController {
+
+	private boolean onLogin;
+	private UserService userService;
+
+	public void initialize(){
+	}
+
+	private void showLogin(){
+	}
+
+	private void showRegister(){
+	}
+
+	private void doLogin(){
+	}
+
+	private void doRegister(){
+	}
+
+	public void onButton(){
+	}
+
+	public void showLoginMessage(){
+	}
+
 }
