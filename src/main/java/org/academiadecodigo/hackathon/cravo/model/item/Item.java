8c3f8fc5ac9c41ca0962e6012fb150e41b62f01c
marianopicco <marianopicco@gmail.com>
2018-04-05 17:51:13 +0100

    added offer and demand classes

--- src/main/java/org/academiadecodigo/hackathon/cravo/model/item/Item.java
+++ src/main/java/org/academiadecodigo/hackathon/cravo/model/item/Item.java
@@ -1,11 +1,9 @@
 package org.academiadecodigo.hackathon.cravo.model.item;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "item")
 public class Item {
 
     @Id
---
+++ src/main/java/org/academiadecodigo/hackathon/cravo/model/transactions/Offer.java
@@ -0,0 +1,67 @@
+package org.academiadecodigo.hackathon.cravo.model.transactions;
+
+import org.academiadecodigo.hackathon.cravo.model.item.Item;
+import org.academiadecodigo.hackathon.cravo.model.user.User;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "offer")
+public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private User user_id;
+
+    @ManyToOne
+    private Item item_id;
+
+    private Integer ammount;
+
+    private Date date;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(User user_id) {
+        this.user_id = user_id;
+    }
+
+    public Item getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(Item item_id) {
+        this.item_id = item_id;
+    }
+
+    public Integer getAmmount() {
+        return ammount;
+    }
+
+    public void setAmmount(Integer ammount) {
+        this.ammount = ammount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
