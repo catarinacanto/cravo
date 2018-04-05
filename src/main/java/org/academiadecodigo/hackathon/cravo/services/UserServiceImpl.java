a51a128957a4df23e13cd278efad37c63c1977b6
João Pires <theflawlessflaw@gmail.com>
2018-04-05 23:36:29 +0100

    added register to UserServiceImpl

--- src/main/java/org/academiadecodigo/hackathon/cravo/services/UserServiceImpl.java
+++ src/main/java/org/academiadecodigo/hackathon/cravo/services/UserServiceImpl.java
@@ -4,23 +4,33 @@ import org.academiadecodigo.hackathon.cravo.model.user.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.HashMap;
-import java.util.Map;
 
 public class UserServiceImpl extends AbstractService<User> {
-
-
-    public UserServiceImpl(EntityManagerFactory emf) {
-        super(emf, User.class);
-    }
-
-
-    public boolean authenticate(String username, String password) {
-
-        EntityManager em = emf.createEntityManager();
-
-        return password.equals(em.find(User.class, username).getPassword());
-    }
+	
+	
+	public UserServiceImpl(EntityManagerFactory emf) {
+		super(emf, User.class);
+	}
+	
+	
+	public boolean authenticate(String username, String password) {
+		
+		EntityManager em = emf.createEntityManager();
+		
+		return password.equals(em.find(User.class, username).getPassword());
+	}
+	
+	
+	public boolean register(User user) {
+		
+		if (user.getUsername() == null) {
+			save(user);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
 
 }
