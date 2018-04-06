a669798c7f592497ced127b27b7710c4a82505bd
Martelo <martelo300@hotmail.com>
2018-04-06 13:13:57 +0100

    more pretty

--- src/main/java/org/academiadecodigo/hackathon/cravo/controllers/FinalController.java
+++ src/main/java/org/academiadecodigo/hackathon/cravo/controllers/FinalController.java
@@ -1,4 +1,17 @@
 package org.academiadecodigo.hackathon.cravo.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.academiadecodigo.hackathon.cravo.views.Navigation;
+
 public class FinalController {
+
+    @FXML
+    private Button itemsButton;
+
+    @FXML
+    void goToItems(ActionEvent event) {
+        Navigation.getInstance().load("currentitems");
+    }
 }
