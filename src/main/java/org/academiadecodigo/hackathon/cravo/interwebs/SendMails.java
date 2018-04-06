95f19247fc50f72005e262ff12fb234b22346b17
João Pires <theflawlessflaw@gmail.com>
2018-04-06 05:25:30 +0100

    ready to send mails hard coded

---
+++ src/main/java/org/academiadecodigo/hackathon/cravo/interwebs/SendMails.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.hackathon.cravo.interwebs;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMails {
+}
